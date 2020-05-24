490c34b0e6ad6c3a55a30ca7c1ea552a8fa3877a
Stephanos Ioannidis <root@stephanos.io>
2020-05-24 20:51:26 +0900

    Add zephyr-crosstool-x86 package
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ zephyr-crosstool-x86/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/stephanosio/zephyr-crosstool-ng/releases/download/zephyr-crosstool-ng-1.24.0.3/zephyr-crosstool-ng-1.24.0.3_windows-x86_64_x86_64-zephyr-elf.zip'
+$archiveDir = "zephyr-crosstool-ng-1.24.0.3"
+
+$packageArgs = @{
+  packageName     = $env:ChocolateyPackageName
+  unzipLocation   = $toolsDir
+  url64bit        = $url64
+  checksum64      = 'E3E100B06F892E8E40E0A4166FEE56763E1AE2D8D23499DC223D327CDA172742'
+  checksumType64  = 'sha256'
+}
+
+# Download and unzip the source package
+Install-ChocolateyZipPackage @packageArgs
+
+# Relocate files out of archive subdirectories
+# HACK: Because Chocolatey PowerShell does not like long paths ...
+cmd /c "xcopy `"$toolsDir\$archiveDir\x86_64-zephyr-elf\*`" `"$toolsDir`" /s /e /i /y /q"
+cmd /c "rmdir /s /q `"$toolsDir\$archiveDir`""
