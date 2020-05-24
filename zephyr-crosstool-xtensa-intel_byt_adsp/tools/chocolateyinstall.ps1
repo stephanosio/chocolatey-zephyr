c8fbfde20ce029dff905a7c72d28cd230dc76b42
Stephanos Ioannidis <root@stephanos.io>
2020-05-24 21:10:03 +0900

    Add zephyr-crosstool-xtensa-intel_byt_adsp package
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ zephyr-crosstool-xtensa-intel_byt_adsp/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/stephanosio/zephyr-crosstool-ng/releases/download/zephyr-crosstool-ng-1.24.0.3/zephyr-crosstool-ng-1.24.0.3_windows-x86_64_xtensa-intel_byt_adsp_zephyr-elf.zip'
+$archiveDir = "zephyr-crosstool-ng-1.24.0.3"
+
+$packageArgs = @{
+  packageName     = $env:ChocolateyPackageName
+  unzipLocation   = $toolsDir
+  url64bit        = $url64
+  checksum64      = '721D6232A6342165168F043047F4EB5A235BA9BCC9CB20825100013297B425B9'
+  checksumType64  = 'sha256'
+}
+
+# Download and unzip the source package
+Install-ChocolateyZipPackage @packageArgs
+
+# Relocate files out of archive subdirectories
+# HACK: Because Chocolatey PowerShell does not like long paths ...
+cmd /c "xcopy `"$toolsDir\$archiveDir\xtensa-intel_byt_adsp_zephyr-elf\*`" `"$toolsDir`" /s /e /i /y /q"
+cmd /c "rmdir /s /q `"$toolsDir\$archiveDir`""
