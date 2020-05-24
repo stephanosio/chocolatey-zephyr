43ec3b3dd2c2c949a9ad2fa4076d4a10337b48f5
Stephanos Ioannidis <root@stephanos.io>
2020-05-24 20:10:28 +0900

    Add zephyr-crosstool-arc package
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ zephyr-crosstool-arc/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/stephanosio/zephyr-crosstool-ng/releases/download/zephyr-crosstool-ng-1.24.0.3/zephyr-crosstool-ng-1.24.0.3_windows-x86_64_arc-zephyr-elf.zip'
+$archiveDir = "zephyr-crosstool-ng-1.24.0.3"
+
+$packageArgs = @{
+  packageName     = $env:ChocolateyPackageName
+  unzipLocation   = $toolsDir
+  url64bit        = $url64
+  checksum64      = '2C9DA22F7F929EA1DEC0ECE83F7B6E89AECB93367D50EBD01E890197D27F19E3'
+  checksumType64  = 'sha256'
+}
+
+# Download and unzip the source package
+Install-ChocolateyZipPackage @packageArgs
+
+# Relocate files out of archive subdirectories
+# HACK: Because Chocolatey PowerShell does not like long paths ...
+cmd /c "xcopy `"$toolsDir\$archiveDir\arc-zephyr-elf\*`" `"$toolsDir`" /s /e /i /y /q"
+cmd /c "rmdir /s /q `"$toolsDir\$archiveDir`""
