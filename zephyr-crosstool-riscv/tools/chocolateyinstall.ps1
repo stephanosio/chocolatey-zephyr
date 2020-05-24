0e13f2437a4e458d0a74d65386272095b3a03010
Stephanos Ioannidis <root@stephanos.io>
2020-05-24 20:33:47 +0900

    Add zephyr-crosstool-riscv package
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ zephyr-crosstool-riscv/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/stephanosio/zephyr-crosstool-ng/releases/download/zephyr-crosstool-ng-1.24.0.3/zephyr-crosstool-ng-1.24.0.3_windows-x86_64_riscv64-zephyr-elf.zip'
+$archiveDir = "zephyr-crosstool-ng-1.24.0.3"
+
+$packageArgs = @{
+  packageName     = $env:ChocolateyPackageName
+  unzipLocation   = $toolsDir
+  url64bit        = $url64
+  checksum64      = '98BCD024C37595155940DE2B8293C22583C462A8F1665F0FED16CAC23B42DEC5'
+  checksumType64  = 'sha256'
+}
+
+# Download and unzip the source package
+Install-ChocolateyZipPackage @packageArgs
+
+# Relocate files out of archive subdirectories
+# HACK: Because Chocolatey PowerShell does not like long paths ...
+cmd /c "xcopy `"$toolsDir\$archiveDir\riscv64-zephyr-elf\*`" `"$toolsDir`" /s /e /i /y /q"
+cmd /c "rmdir /s /q `"$toolsDir\$archiveDir`""
