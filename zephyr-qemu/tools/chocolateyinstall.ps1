bfb1570a284eee7cd8be6663d92476af6391f6b3
Stephanos Ioannidis <root@stephanos.io>
2020-05-26 17:46:10 +0900

    Add zephyr-qemu package
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ zephyr-qemu/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/stephanosio/zephyr-qemu/releases/download/zephyr-qemu-v5.0.0.0/zephyr-qemu-v5.0.0.0_windows-x86_64.zip'
+$archiveDir = "zephyr-qemu-v5.0.0.0"
+
+$packageArgs = @{
+  packageName     = $env:ChocolateyPackageName
+  unzipLocation   = $toolsDir
+  url64bit        = $url64
+  checksum64      = '1AF73917455A21663B46C9575051D364A49E9FF656DDAC5FE8F63B88D1D2D0A8'
+  checksumType64  = 'sha256'
+}
+
+# Download and unzip the source package
+Install-ChocolateyZipPackage @packageArgs
+
+# Relocate files out of archive subdirectories
+# HACK: Because Chocolatey PowerShell does not like long paths ...
+cmd /c "xcopy `"$toolsDir\$archiveDir\*`" `"$toolsDir`" /s /e /i /y /q"
+cmd /c "rmdir /s /q `"$toolsDir\$archiveDir`""
